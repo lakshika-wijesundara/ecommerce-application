4870026e1f291c67c1d403da65db0665448968a6
lakshika wijesundara <lakshika2wijesundara@gmail.com>
2026-01-25 13:38:03 +0530

    Add Product model as initial commit

---
+++ ecom-project/ecom-project/src/main/java/com/prac/ecom_project/controller/ProductController.java
@@ -0,0 +1,11 @@
+package com.prac.ecom_project.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+
+}
