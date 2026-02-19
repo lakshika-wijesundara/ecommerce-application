eac4d23aef6acbf5e5864e924b103e4c55b32282
lakshika wijesundara <lakshika2wijesundara@gmail.com>
2026-02-19 23:46:22 +0530

    Add UserService interface file

--- ecom-project/ecom-project/src/main/java/com/prac/ecom_project/service/UserService.java
+++ ecom-project/ecom-project/src/main/java/com/prac/ecom_project/service/UserService.java
@@ -1,2 +1,20 @@
-package com.prac.ecom_project.service;public interface UserService {
+package com.prac.ecom_project.service;
+
+import com.prac.ecom_project.model.Product;
+import com.prac.ecom_project.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    User createUser(User user);
+
+    List<User> getAllUsers();
+
+    User findUserById(Long id);
+
+    User updateUserById(Long id, User user);
+
+    User deleteUserById(Long id, User user);
+
 }
