7eb7918ae2b308bd327e9a1dbe4f0fa7c043d8ba
lakshika wijesundara <lakshika2wijesundara@gmail.com>
2026-02-19 23:42:20 +0530

    Add ProductService interface file

--- ecom-project/ecom-project/src/main/java/com/prac/ecom_project/service/ProductService.java
+++ ecom-project/ecom-project/src/main/java/com/prac/ecom_project/service/ProductService.java
@@ -1,2 +1,20 @@
-package com.prac.ecom_project.service;public interface ProductService {
+package com.prac.ecom_project.service;
+
+import com.prac.ecom_project.model.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    Product createProduct(Product product);
+
+    List<Product> getAllProducts();
+
+    Product getProductById(Long id);
+
+    Product updateProduct(Long id,Product product);
+
+    Product deleteProduct(Long id);
+
+    List<Product> searchProducts(String keyword);
 }
