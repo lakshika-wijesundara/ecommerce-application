b8ff7051b2e77f3ee6d04c1f310eeb6089cd2e6d
lakshika wijesundara <lakshika2wijesundara@gmail.com>
2026-04-06 02:11:01 +0530

    Add order interface file

--- ecom-project/ecom-project/src/main/java/com/prac/ecom_project/service/OrderService.java
+++ ecom-project/ecom-project/src/main/java/com/prac/ecom_project/service/OrderService.java
@@ -1,4 +1,21 @@
 package com.prac.ecom_project.service;
 
-public interface OrderService {
+import com.prac.ecom_project.model.Order;
+
+import java.util.List;
+
+public interface OrderService
+{
+    Order createOder(Order order);
+
+    List<Order> getAllOrders();
+
+    Order updateOrder(Long id,Order order);
+
+    Order deleteOrder(Long id);
+
+    List<Order> searchOrders(String keyword);
+
+
+
 }
